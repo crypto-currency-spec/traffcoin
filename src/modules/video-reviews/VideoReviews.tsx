88f0c440b12e61e0f4482d4fdceaec6d45b95e64
Yehor-f2006 <151622806+Yehor-f2006@users.noreply.github.com>
2024-06-28 23:53:05 +0200

    updated video-reviews section

--- src/modules/video-reviews/VideoReviews.tsx
+++ src/modules/video-reviews/VideoReviews.tsx
@@ -11,10 +11,10 @@ export const VideoReviews = () => {
 
   const videos = [
     "https://player.vimeo.com/video/958989546?h=d4f32ab887",
-    "https://player.vimeo.com/video/958996733?h=ed70a1e551",
-    "https://player.vimeo.com/video/959002387?h=fbcab6a183",
     "https://player.vimeo.com/video/959013129?h=c27ffb1585",
+    "https://player.vimeo.com/video/958996733?h=ed70a1e551",
     "https://player.vimeo.com/video/959019905?h=2d8d47d419",
+    "https://player.vimeo.com/video/959002387?h=fbcab6a183",
     "https://player.vimeo.com/video/959021703?h=baed1d1492",
   ];
 
