8450dcf91e38517de4998d1f0caed94e36624f88
Yehor-f2006 <151622806+Yehor-f2006@users.noreply.github.com>
2024-06-30 23:00:18 +0200

    updated reviews

--- src/modules/reviews/Reviews.tsx
+++ src/modules/reviews/Reviews.tsx
@@ -4,16 +4,26 @@ import { Scroller } from "../scroller/Scroller";
 export const Reviews = () => {
   const { data } = useAppSelector(state => state.data);
   const images = [
-    'images/reviews/review-1.jpeg',
-    'images/reviews/review-2.jpeg',
-    'images/reviews/review-3.jpeg',
-    'images/reviews/review-4.jpeg',
-    'images/reviews/review-5.jpeg',
-    'images/reviews/review-6.jpeg',
-    'images/reviews/review-7.jpeg',
-    'images/reviews/review-8.jpeg',
-    'images/reviews/review-9.jpeg',
-    'images/reviews/review-10.jpeg',
+    'images/reviews/review-1.jpg',
+    'images/reviews/review-2.jpg',
+    'images/reviews/review-3.jpg',
+    'images/reviews/review-4.jpg',
+    'images/reviews/review-5.jpg',
+    'images/reviews/review-6.jpg',
+    'images/reviews/review-7.jpg',
+    'images/reviews/review-8.jpg',
+    'images/reviews/review-9.jpg',
+    'images/reviews/review-10.jpg',
+    'images/reviews/review-11.jpeg',
+    'images/reviews/review-12.jpeg',
+    'images/reviews/review-13.jpeg',
+    'images/reviews/review-14.jpeg',
+    'images/reviews/review-15.jpeg',
+    'images/reviews/review-16.jpeg',
+    'images/reviews/review-17.jpeg',
+    'images/reviews/review-18.jpeg',
+    'images/reviews/review-19.jpeg',
+    'images/reviews/review-20.jpeg',
   ];
 
   return (
